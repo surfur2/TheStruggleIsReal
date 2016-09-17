61a06ba115e373e0776ba5f5b4fc32c46f2ea116
WAVEBREAKER\NateBlair <nategribble@gmail.com>
2016-09-17 18:57:54 -0400

    Initial player prefab

---
+++ TheStruggleIsReal/Assets/Scripts/Player.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+    public float moveSpeed = 6.0f; // Default move speed
+    private Rigidbody2D rgb2d; // Used for moving the charcter
+
+    // For later use
+    //private Animator animator;
+
+    // Use this for initialization
+	void Start () {
+        rgb2d = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+        float horzMove = Input.GetAxis("Horizontal");
+        float vertMove = Input.GetAxis("Vertical");
+
+        rgb2d.velocity = new Vector2(horzMove * moveSpeed, vertMove * moveSpeed);
+    }
+}
