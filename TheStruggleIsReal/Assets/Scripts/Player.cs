79e73dd1bf218054b5ce3a4967912099a1ba9d15
rahulmadhyan <rahulsmadhy@gmail.com>
2016-09-18 01:28:49 -0400

    Projectile. Player Shoot.

--- TheStruggleIsReal/Assets/Scripts/Player.cs
+++ TheStruggleIsReal/Assets/Scripts/Player.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class Player : MonoBehaviour
 {
+    public GameObject energyBar;
     public float moveSpeed = 6.0f; // Default move speed
 
     private Rigidbody2D rgb2d; // Used for moving the charcter
@@ -16,6 +17,43 @@ public class Player : MonoBehaviour
     {
         rgb2d = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        Vector3 shootPosition;
+
+        if(Input.GetKeyDown("right"))
+        {
+            shootPosition = new Vector3(transform.position.x + 0.5f, transform.position.y, 0.0f);    
+            GameObject eBar = Instantiate(energyBar, shootPosition, Quaternion.identity) as GameObject;
+            Projectile eBarProjectile = eBar.GetComponent<Projectile>();
+            eBarProjectile.moveDirection = 1;
+        }
+
+        if (Input.GetKeyDown("left"))
+        {
+            shootPosition = new Vector3(transform.position.x - 0.5f, transform.position.y, 0.0f);
+            GameObject eBar = Instantiate(energyBar, shootPosition, Quaternion.identity) as GameObject;
+            Projectile eBarProjectile = eBar.GetComponent<Projectile>();
+            eBarProjectile.moveDirection = -1;
+        }
+
+        if (Input.GetKeyDown("up"))
+        {
+            shootPosition = new Vector3(transform.position.x, transform.position.y + 0.5f, 0.0f);
+            GameObject eBar = Instantiate(energyBar, shootPosition, Quaternion.identity) as GameObject;
+            Projectile eBarProjectile = eBar.GetComponent<Projectile>();
+            eBarProjectile.moveDirection = 2;
+        }
+
+        if (Input.GetKeyDown("down"))
+        {
+            shootPosition = new Vector3(transform.position.x, transform.position.y - 0.5f, 0.0f);
+            GameObject eBar = Instantiate(energyBar, shootPosition, Quaternion.identity) as GameObject;
+            Projectile eBarProjectile = eBar.GetComponent<Projectile>();
+            eBarProjectile.moveDirection = -2;
+        }
     }
 
     // Update is called once per frame
@@ -39,6 +77,5 @@ public class Player : MonoBehaviour
         {
             anim.SetBool("walkright", false);
         }
-
     }
 }
---
+++ TheStruggleIsReal/Assets/Scripts/Projectile.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Projectile : MonoBehaviour
+{
+    public int moveDirection;
+    public float moveSpeed = 100.0f;
+    public int killDelay = 2;
+   
+    // Use this for initialization
+    void Start()
+    {
+        Kill();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(moveDirection == 1)
+        transform.position = new Vector3(transform.position.x + (moveSpeed * Time.deltaTime), transform.position.y, 0.0f);
+
+        if (moveDirection == -1)
+            transform.position = new Vector3(transform.position.x - (moveSpeed * Time.deltaTime), transform.position.y, 0.0f);
+
+        if (moveDirection == 2)
+            transform.position = new Vector3(transform.position.x, transform.position.y + (moveSpeed * Time.deltaTime), 0.0f);
+
+        if (moveDirection == -2)
+            transform.position = new Vector3(transform.position.x, transform.position.y - (moveSpeed * Time.deltaTime), 0.0f);
+    }
+
+    void Kill()
+    {
+        Destroy(gameObject, killDelay);
+    }
+}
