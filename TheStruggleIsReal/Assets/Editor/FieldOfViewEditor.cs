5bbaa40008cacb9a2809d61dafbf0f12438f2775
WAVEBREAKER\NateBlair <nategribble@gmail.com>
2016-09-18 22:57:59 -0400

    Trying to get the line of sight for player to work

---
+++ TheStruggleIsReal/Assets/Editor/FieldOfViewEditor.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor (typeof (FieldOfView))]
+public class FieldOfViewEditor : Editor {
+
+	void OnSceneGUI()
+    {
+        FieldOfView fov = (FieldOfView)target;
+        Handles.color = Color.white;
+        Handles.DrawWireArc(fov.transform.position, Vector3.forward, Vector3.right, 360, fov.viewRadius);
+
+        Vector3 viewAngleA = fov.DirFromAngle(-fov.viewAngle / 2);
+        Vector3 viewAngleB = fov.DirFromAngle(fov.viewAngle / 2);
+
+        Handles.DrawLine(fov.transform.position, fov.transform.position + viewAngleA * fov.viewRadius);
+        Handles.DrawLine(fov.transform.position, fov.transform.position + viewAngleB * fov.viewRadius);
+    }
+}
---
+++ TheStruggleIsReal/Assets/Scripts/FieldOfView.cs
@@ -0,0 +1,102 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class FieldOfView : MonoBehaviour
+{
+
+    public float viewRadius;
+    public float viewAngle;
+
+    public float meshResolution;
+
+    public MeshFilter viewMeshFilters;
+    Mesh viewMesh;
+
+    void Start()
+    {
+        viewMesh = new Mesh();
+        viewMesh.name = "View Mesh";
+        viewMeshFilters.mesh = viewMesh;
+        StartCoroutine("FindTargestWithDelay", .2f);
+    }
+
+    public Vector3 DirFromAngle(float angleInDegrees)
+    {
+        return new Vector3(Mathf.Sin(angleInDegrees * Mathf.Deg2Rad), Mathf.Cos(angleInDegrees * Mathf.Deg2Rad), 0);
+    }
+
+    void DrawFieldOfView()
+    {
+        int stepCount = Mathf.RoundToInt(viewAngle * meshResolution);
+        float stepAngleSize = viewAngle / stepCount;
+        List<Vector3> viewPoints = new List<Vector3>();
+        for (int i=0; i <= stepCount; i++)
+        {
+            float angle = transform.eulerAngles.z - viewAngle / 2 + stepAngleSize * i;
+            // Debug.DrawLine(transform.position, transform.position + DirFromAngle(angle) * viewRadius, Color.red); 
+            ViewCastInfo newViewCast = ViewCast(angle);
+
+            viewPoints.Add(newViewCast.point);
+        }
+
+        int vertexCount = viewPoints.Count + 1;
+        Vector3[] verticies = new Vector3[vertexCount];
+        int[] triangles = new int[(vertexCount - 2) * 3];
+
+        verticies[0] = Vector3.zero;
+        for (int i=0; i < vertexCount - 1; i++)
+        {
+            verticies[i + 1] = viewPoints[i];
+
+            if (i < vertexCount - 2)
+            {
+                triangles[i * 3] = 0;
+                triangles[i * 3 + 1] = i + 1;
+                triangles[i * 3 + 2] = i + 2;
+            }
+       }
+
+        viewMesh.Clear();
+        viewMesh.vertices = verticies;
+        viewMesh.triangles = triangles;
+        viewMesh.RecalculateNormals();
+    }
+
+    ViewCastInfo ViewCast(float angle)
+    {
+        Vector3 dir = DirFromAngle(angle);
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, dir, viewRadius, 6, 0, viewRadius);
+        // Physics.Raycast(transform.position, dir, out hit, viewRadius, 6);
+
+        // if (Physics.Raycast(transform.position, dir, out hit, viewRadius, 6))
+        if (hit.collider != null)
+        {
+            return new ViewCastInfo(true, hit.point, hit.distance, angle);
+        }
+        else
+        {
+            return new ViewCastInfo(false, transform.position + dir * viewRadius, viewRadius, angle);
+        }
+    }
+    void Update()
+    {
+        DrawFieldOfView();
+    }
+
+    public struct ViewCastInfo
+    {
+        public bool hit;
+        public Vector3 point;
+        public float dst;
+        public float angle;
+
+        public ViewCastInfo(bool _hit, Vector3 _point, float _dst, float _angle)
+        {
+            hit = _hit;
+            point = _point;
+            dst = _dst;
+            angle = _angle;
+        }
+    }
+}
